46f9ad8f42c7b323e609a9d66d511eaedb656c4c
Stephen Parton <stephen.parton@gmail.com>
2025-09-30 10:24:57 +1000

    Add initial Jupyter notebook with a test markdown cell

---
+++ src/dataset.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    "Name": ["Alice", "Bob", "Charlie"],
+    "Age": [25, 30, 35],
+    "City": ["New York", "San Francisco", "Los Angeles"],
+}
+
+df = pd.DataFrame(data)
+
+df.replace("Alice", "Alicia", inplace=True)
